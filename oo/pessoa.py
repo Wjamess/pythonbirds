d800d7455ed46cdf471d2826fe0a1b1cec8e3c4f
Wjamess <wjmcardoso.93@gmail.com>
2021-11-25 21:43:53 -0300

    Criada o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,11 @@
 # Classes devem começar com letra maiusculo ex ExemploPessoa:
 
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
